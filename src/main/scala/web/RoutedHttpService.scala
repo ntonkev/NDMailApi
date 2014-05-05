be19169124258a2fb40ebf48b43728b688bdfa74
Nikola Tonkev <nikola_tonkev@yahoo.com>
2014-05-05 00:42:07 -0400

    Code cleaned up in RoutedHttpService

--- src/main/scala/web/RoutedHttpService.scala
+++ src/main/scala/web/RoutedHttpService.scala
@@ -16,12 +16,6 @@ class RoutedHttpService(route: Route) extends Actor with HttpService with SprayA
   implicit val NDResponseFormater = jsonFormat3(NDApiResponse[String])
 
   implicit def actorRefFactory = context
-  /*
-  implicit val NDRejectionHandler = RejectionHandler{
-    case AuthenticationFailedRejection(realm) :: _ =>
-      complete(Unauthorized, "This request have not been authorized")
-  }
-  */
 
   implicit val NDRejectionHandler = RejectionHandler{
     case AuthenticationFailedRejection(realm) :: _ => ctx => {
@@ -42,8 +36,6 @@ class RoutedHttpService(route: Route) extends Actor with HttpService with SprayA
 
   implicit val rejectionHandler = NDRejectionHandler// orElse RejectionHandler.Default
 
-//  def receive: Receive =
-//    runRoute(route)(handler, RejectionHandler.Default, context, RoutingSettings.default, LoggingContext.fromActorRefFactory)
   def receive: Receive =
     runRoute(route)(handler, rejectionHandler, context, RoutingSettings.default, LoggingContext.fromActorRefFactory)
 }
